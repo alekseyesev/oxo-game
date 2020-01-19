44e63ca87a784f1bc46630bd2ebb95da8b65ee76
alekseyesev <alekseyesev@live.ru>
2020-01-19 23:32:13 +0300

    Story 5: Made interface ICellProps properties optional

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="react-scripts" />
 
 interface ICellProps {
-    active: boolean,
-    oPlayer: boolean,
-    xPlayer: boolean
+    active?: boolean,
+    oPlayer?: boolean,
+    xPlayer?: boolean
 }
