9d14773e67e11050ce59452cd1ba56e9b81473a5
alekseyesev <alekseyesev@live.ru>
2020-01-21 01:08:57 +0300

    Story 7: Add interface ICellClassesProps

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,16 @@
 /// <reference types="react-scripts" />
 
-interface ICellProps {
+interface ITableProps {
+    inProgress?: boolean,
+    placeMark(markIndex: number): void,
+    cells: Array<ICellProps>
+}
+
+interface ICellProps extends ICellClassesProps {
+    onClick(): void
+}
+
+interface ICellClassesProps {
     active?: boolean,
     oPlayer?: boolean,
     xPlayer?: boolean
