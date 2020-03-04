d9ea82ec90908b1eef86927c82a98697decc2398
alekseyesev <alekseyesev@live.ru>
2020-03-04 23:42:24 +0300

    Story 18: Add component WithScore

---
+++ src/components/hoc/WithScore.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+
+const WithScore: Function = (
+  Component: React.FC<any>,
+  playerId: playerId
+): React.FC<IPlayerProps> => {
+  const scoreSelector = createSelector(
+    [(state: store): IEvents => state.events],
+    (events): number => events[playerId].score
+  );
+
+  const ComponentWithScore: React.FC<IPlayerProps> = componentProps => {
+    const score: number = useSelector(scoreSelector);
+
+    return <Component score={score} {...componentProps} />;
+  };
+  return ComponentWithScore;
+};
+
+export default WithScore;
