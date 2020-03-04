97bb6ba43c6c69edab52e07761797fdceaef2ea9
alekseyesev <alekseyesev@live.ru>
2020-03-04 23:55:37 +0300

    Story: 19: Apply hoc-component WithScore in component Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,18 @@
 import React from "react";
+import Player from "./Player";
+import WithScore from "./hoc/WithScore";
 import { ReactComponent as Logo } from "../logo.svg";
 
 const Header: React.FC<{}> = () => {
+  const PlayerOWithScore: React.FC<IPlayerProps | {}> = WithScore(Player, "o");
+  const PlayerXWithScore: React.FC<IPlayerProps | {}> = WithScore(Player, "x");
   return (
     <header className="oxo__header">
-      <div className="player">
-        <div className="player__name">Игрок O</div>
-        <div className="player__score">0</div>
-      </div>
+      <PlayerOWithScore />
       <div className="oxo__logo">
         <Logo />
       </div>
-      <div className="player">
-        <div className="player__name">Игрок X</div>
-        <div className="player__score">0</div>
-      </div>
+      <PlayerXWithScore />
     </header>
   );
 };
